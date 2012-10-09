dd5ac4887688dbaa8df5d8606f3e72b76a64b3a6
Robin Tibor Schirrmeister <robintibor@gmail.com>
2012-10-09 15:31:09 +0200

    first working version with usable interface showing first results

--- coffee/search-ui.coffee
+++ coffee/search-ui.coffee
@@ -1,4 +1,3 @@
-
 queryServer = (queryString) ->
     $.ajax(
         {url: 'http://metropolis.informatik.uni-freiburg.de:28451/',
@@ -6,8 +5,36 @@ queryServer = (queryString) ->
         dataType: 'jsonp',
         success: (data) ->
             $('#resultDiv').html("<pre> #{JSON.stringify(data, '<br/>', '\t')} </pre>")
+            resultHTML = createResultHTML(data)
+            addResultHTMLToResultDiv(resultHTML)
         }
     )
+
+createResultHTML = (data) ->
+    resultDocumentsHTML = ""
+    for doc in data.documents
+        resultDocumentsHTML += createResultHTMLForDocument(doc)
+    return resultDocumentsHTML
+
+createResultHTMLForDocument = (doc) ->
+    snippetHTML = createSnippetsHTML(doc)
+    return "<div class='oneResult'>
+            <div class='resultHeader'><span class='resultTitle'>#{doc.title}</span><span class='resultScore'>#{doc.score}</span></div>
+            <div>#{snippetHTML}</div>
+            </div>"
+
+createSnippetsHTML = (doc) ->
+    snippetHTML = ""
+    for snippet in doc.snippets
+        snippetHTML += createSnippetHTML(snippet)
+    return snippetHTML
+
+createSnippetHTML = (snippet) ->
+    return "<div class='snippet'><span class='fieldName'>#{snippet.fieldName}</span><div class='snippetText'>#{snippet.text}</div></div>"
+
+addResultHTMLToResultDiv = (resultHTML) ->
+    $('#resultDiv').prepend(resultHTML)
+
 $('#queryForm').submit(() -> 
     queryServer($('#queryInput').val())
     return false
--- js/search-ui.js
+++ js/search-ui.js
@@ -1,5 +1,5 @@
 (function() {
-  var queryServer;
+  var addResultHTMLToResultDiv, createResultHTML, createResultHTMLForDocument, createSnippetHTML, createSnippetsHTML, queryServer;
 
   queryServer = function(queryString) {
     return $.ajax({
@@ -10,9 +10,48 @@
       },
       dataType: 'jsonp',
       success: function(data) {
-        return $('#resultDiv').html("<pre> " + (JSON.stringify(data, '<br/>', '\t')) + " </pre>");
+        var resultHTML;
+        $('#resultDiv').html("<pre> " + (JSON.stringify(data, '<br/>', '\t')) + " </pre>");
+        resultHTML = createResultHTML(data);
+        return addResultHTMLToResultDiv(resultHTML);
       }
     });
+  };
+
+  createResultHTML = function(data) {
+    var doc, resultDocumentsHTML, _i, _len, _ref;
+    resultDocumentsHTML = "";
+    _ref = data.documents;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      doc = _ref[_i];
+      resultDocumentsHTML += createResultHTMLForDocument(doc);
+    }
+    return resultDocumentsHTML;
+  };
+
+  createResultHTMLForDocument = function(doc) {
+    var snippetHTML;
+    snippetHTML = createSnippetsHTML(doc);
+    return "<div class='oneResult'>            <div class='resultHeader'><span class='resultTitle'>" + doc.title + "</span><span class='resultScore'>" + doc.score + "</span></div>            <div>" + snippetHTML + "</div>            </div>";
+  };
+
+  createSnippetsHTML = function(doc) {
+    var snippet, snippetHTML, _i, _len, _ref;
+    snippetHTML = "";
+    _ref = doc.snippets;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      snippet = _ref[_i];
+      snippetHTML += createSnippetHTML(snippet);
+    }
+    return snippetHTML;
+  };
+
+  createSnippetHTML = function(snippet) {
+    return "<div class='snippet'><span class='fieldName'>" + snippet.fieldName + "</span><div class='snippetText'>" + snippet.text + "</div></div>";
+  };
+
+  addResultHTMLToResultDiv = function(resultHTML) {
+    return $('#resultDiv').prepend(resultHTML);
   };
 
   $('#queryForm').submit(function() {
